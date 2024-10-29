54dd7af5faa57060cd6bd9ada48933113b7bf940
ivancamilo911 <ivancamilopizo@iemmmrip.edu.co>
2024-10-29 17:44:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var na=5;
+function comprobar(){
+    var nu=document.getElementById("nose").value;
+    if(na==nu){
+        alert("ganaste");
+    }else if(na>nu){
+       alert("es mayor"); 
+    }else{
+        alert("es menor");
+    };
+      {
+       "user":{
+       "name": "Nombre del usuario",
+       "age": "edad del usuario", 
+    } 
+}
